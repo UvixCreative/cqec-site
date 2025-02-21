e82cfec39f41cf0d6d16d2d1c793b61d06b85be7
Ben Rankin <me@benrank.in>
2025-02-21 18:06:52 -0500

    Tried adding local URL to site config

--- _config.ts
+++ _config.ts
@@ -2,11 +2,13 @@ import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
 import googleFonts from "lume/plugins/google_fonts.ts";
 
-const site = lume();
+const site = lume({
+    location: new URL("https://chattqueerempowerment.org"),
+});
 
 site.use(blog());
 site.use(googleFonts({
-  fonts:
+    fonts:
     "https://fonts.googleapis.com/css2?family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&family=Yeseva+One&display=swap",
 }));
 site.copy("/assets");
